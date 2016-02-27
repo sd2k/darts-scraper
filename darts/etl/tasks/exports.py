2230dccf314811c2874547ed770030d4e567b526
Ben Sully <ben.sully@yougov.com>
2016-02-27 17:45:07 +0000

    Specify columns in UpcomingPlayerStatsSlackReport

--- darts/etl/tasks/exports.py
+++ darts/etl/tasks/exports.py
@@ -18,18 +18,22 @@ class UpcomingPlayerStatsSlackReport(
     @property
     def query(self):
         return textwrap.dedent("""
-            SELECT string_agg(p.name, ' vs ') AS fixture_name,
+            SELECT string_agg(p.name, ' vs ') OVER (PARTITION BY f.id) AS fixture_name,
                 f.date AS fixture_date,
                 e.name AS fixture_event,
-                stats.*,
-                p.*
+                stats.player_name,
+                stats.oneeighties,
+                stats.legs,
+                stats.oneeighties_per_leg,
+                p.pdc_ranking,
+                p.career_9_darters
             FROM fixtures AS f
             JOIN fixture_players AS fp ON f.id = fp.fixture_id
             JOIN {stats_table} AS stats ON fp.player_id = stats.player_id
             JOIN players AS p ON fp.player_id = p.id
             JOIN events AS e ON f.event_id = e.id
             WHERE f.date - CURRENT_DATE <= 7
-        """).strip().format(
+        """).strip().format(  # noqa
             stats_table=self.input().table
         )
 
