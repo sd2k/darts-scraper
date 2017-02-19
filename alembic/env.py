6ac39574f670489e0f34203897079e53df6e9a89
Ben Sully <ben.sully@yougov.com>
2017-02-19 22:07:11 +0000

    Point alembic to correct database URL

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,13 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
+from dartssim import db, model, settings  # noqa
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -15,8 +19,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from darts import models
-target_metadata = models.Base.metadata
+target_metadata = db.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
