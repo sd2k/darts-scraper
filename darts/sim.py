1090ceb334235e52c1929d3464a72546e32570fa
Ben Sully <ben.sully@yougov.com>
2017-02-25 17:59:22 +0000

    Add docstrings to sim module

--- darts/sim.py
+++ darts/sim.py
@@ -1,6 +1,9 @@
-import random
+"""
+Simulations of 1-player matches.
 
-import numpy as np
+TODO: Add number of 180s.
+"""
+import random
 
 from darts import models
 
@@ -13,6 +16,15 @@ DEFAULT_POINTS = (60, 20, 6)
 
 
 def load_lookups(session):
+    """
+    Fetch the two required lookup tables from the database.
+
+    Returns a 2-tuple containing:
+
+    - a mapping from (score, dart ID) to shot type
+    - a mapping from (score, dart ID) to a three-tuple containing
+        (hit score, miss score, big miss score)
+    """
     rows = session.query(models.ScoreLookup)
     score_shot_types = {}
     score_points = {}
@@ -29,6 +41,14 @@ def load_lookups(session):
 
 
 def get_shot_type(current_score, dart_number, score_shot_types):
+    """
+    Get the ideal shot for a given score and dart ID.
+
+    :param int current_score:
+    :param int dart_number: the dart ID (1, 2, or 3)
+    :param dict score_shot_types: mapping from (score, dart_number) to
+        shot types, as returned by `~load_lookups`
+    """
     return score_shot_types.get(
         (current_score, dart_number),
         DEFAULT_SHOT_TYPE,
@@ -37,8 +57,21 @@ def get_shot_type(current_score, dart_number, score_shot_types):
 
 def get_result(shot_type, profile):
     """
-    Return 0 for hit, 1 for miss OR miss inside,
-    2 for big miss OR miss outside.
+    Return a random result for a shot type, with the probability
+    determined by the given profile.
+
+    For example, if the shot type is 'treble', the result will
+    be either 0, 1 or 2 depending on the profile's 'treble' probabilities.
+
+    :param str shot_type: type of shot, either 'treble', 'single', 'bull',
+        'outer_bull', or 'double'
+
+    :param profile: shot profile
+    :type profile: :py:class:`~darts.models.Profile`
+
+    :return: shot outcome - 0 for hit, 1 for miss (or miss inside),
+        2 for big miss (or miss outside)
+    :rtype: int
     """
 
     if shot_type == 'single':
@@ -79,6 +112,21 @@ def get_result(shot_type, profile):
 
 
 def get_points(current_score, dart_number, result, score_points):
+    """
+    Return the points scored by a shot.
+
+    Given the current score, dart ID and shot result, get the number
+    of points scored by such a shot.
+
+    :param int current_score: current score
+    :param int dart_number: dart ID (1, 2, 3)
+    :param string result: shot result, as returned by get_result.
+    :param dict score_points: mapping from (score, dart ID) to score tuple,
+        as returned by `load_lookups`.
+
+    :return: number of points scored.
+    :rtype: int
+    """
     point_tuple = score_points.get(
         (current_score, dart_number),
         DEFAULT_POINTS,
@@ -93,6 +141,25 @@ def throw_dart(
         score_shot_types,
         score_points,
         ):
+    """
+    Simulate the throwing of a dart at a particular score.
+
+    Returns the number of points scored by the dart.
+
+    :param int current_score: current score (before throwing the dart)
+    :param int dart_number: dart ID (1, 2, 3)
+
+    :param profile: shot profile
+    :type profile: :py:class:`~darts.models.Profile`
+
+    :param dict score_shot_types: mapping from (score, dart_number) to
+        shot types, as returned by `~load_lookups`
+    :param dict score_points: mapping from (score, dart ID) to score tuple,
+        as returned by `load_lookups`.
+
+    :return: number of points scored.
+    :rtype: int
+    """
     shot_type = get_shot_type(current_score, dart_number, score_shot_types)
     result = get_result(shot_type, profile)
     points = get_points(current_score, dart_number, result, score_points)
@@ -101,7 +168,29 @@ def throw_dart(
 
 def throw_three_darts(current_score, profile, score_shot_types, score_points):
     """
-    Returns (new_score, three_dart_total, n_darts).
+    Simulate throwing three darts.
+
+    Encapsulates the logic of winning if score reaches 0 after any dart,
+    or busting if score is either 1 or less than zero.
+
+    Returns a tuple containing:
+
+    - the new score after throwing three darts
+    - the three dart total
+    - the number of darts thrown
+
+    :param int current_score: current score (before throwing the dart)
+
+    :param profile: shot profile
+    :type profile: :py:class:`~darts.models.Profile`
+
+    :param dict score_shot_types: mapping from (score, dart_number) to
+        shot types, as returned by `~load_lookups`
+    :param dict score_points: mapping from (score, dart ID) to score tuple,
+        as returned by `load_lookups`.
+
+    :return: the new score, three dart total, and number of darts thrown.
+    :rtype: Tuple[int, int, int]
     """
 
     new_score = current_score
@@ -134,6 +223,21 @@ def throw_three_darts(current_score, profile, score_shot_types, score_points):
 
 def simulate_leg(profile, score_shot_types, score_points, total=501):
     """
+    Simulate a leg of darts.
+
+    Returns every three dart total.
+
+    :param profile: shot profile
+    :type profile: :py:class:`~darts.models.Profile`
+
+    :param dict score_shot_types: mapping from (score, dart_number) to
+        shot types, as returned by `~load_lookups`
+    :param dict score_points: mapping from (score, dart ID) to score tuple,
+        as returned by `load_lookups`.
+    :param int total: Total required to win a leg of darts (default: 501).
+
+    :return: List of three dart totals.
+    :rtype: List[int]
     """
     three_dart_totals = []
     current_score = total
@@ -157,6 +261,18 @@ def simulate_profile(
         total=501,
         ):
     """
+    Simulate a profile by simulating lots of legs.
+
+    :param profile: shot profile
+    :type profile: :py:class:`~darts.models.Profile`
+
+    :param dict score_shot_types: mapping from (score, dart_number) to
+        shot types, as returned by `~load_lookups`
+    :param dict score_points: mapping from (score, dart ID) to score tuple,
+        as returned by `load_lookups`.
+    :param int iterations: Number of iterations to run (default: 10000).
+    :param int total: Total required to win a leg of darts (default: 501).
+
     """
     three_dart_totals = []
     for i in xrange(iterations):
